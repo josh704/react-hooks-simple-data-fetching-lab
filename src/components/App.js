f912037bf23724ad7bf2a0749eefb7464da23feb
josh704 <joshuangetich02@gmail.com>
2024-11-04 23:47:25 +0300

    data-featching-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+function App() {
+   const [dogsImage, setDogImage] = useState(null)
+   const [loading, setLoading] = useState(true)
+
+   useEffect(() => {
+    fetch ("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((data) => {
+        setDogImage(data.message)
+        setLoading(false)
+    })
+   }, []);
+   if(loading) {
+    return <p>Loading...</p>
+   }
+   return (
+    <div>
+        <img src={dogsImage} alt="A Random Dog" />
+    </div>
+   )
+}
+export default App;
